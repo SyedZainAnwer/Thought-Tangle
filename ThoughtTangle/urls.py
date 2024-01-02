ed4eada0a19b5483d07287d12ac0f6bd7d00de2f
Syed Zain Anwer <zainanwer24@gmail.com>
2024-01-02 09:40:39 +0100

    first commit

---
+++ ThoughtTangle/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+#urls trigger views and that what we see on the server
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')) # any path that matches and empty string, go to base.urls file
+]
