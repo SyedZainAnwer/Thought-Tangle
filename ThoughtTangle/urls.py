31d708216bf231d6a29eba52f1f6f1018fe4b7e6
Syed Zain Anwer <zainanwer24@gmail.com>
2024-01-03 02:34:06 +0100

    added and learnt how template works

--- ThoughtTangle/urls.py
+++ ThoughtTangle/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-#urls trigger views and that what we see on the server
+# urls trigger views and that what we see on the server
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')) # any path that matches and empty string, go to base.urls file
--- base/views.py
+++ base/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# render method takes 2 parameter and 2 optional: 1 is the HTTP request and other is the template
+
+rooms = [
+    { 'id': 1, 'name': 'Lets learn Django' },
+    { 'id': 2, 'name': 'Design with me' },
+    { 'id': 3, 'name': 'Frontend Developers' }
+]
 
 def home(request):
-    return HttpResponse('Home page')
+    context = {'rooms': rooms} # here we are getting all dictionaries in a list
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('Room page')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i # here room will be equal to id, as in the urls.py room/<pk> == room/id
+    context = {'room': room} # here we are getting each single room according to it's id i.e each single dictionary
+        
+    return render(request, 'base/room.html', context)
