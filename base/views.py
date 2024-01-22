36659aee90db5d2bf47b2eb7bf8640d9c96b8f28
Syed Zain Anwer <zainanwer24@gmail.com>
2024-01-22 11:01:36 +0100

    fix upate/create room page

--- base/views.py
+++ base/views.py
@@ -120,12 +120,16 @@ def createRoom(request):
     topics = Topic.objects.all()
     
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user # host will be the logged in user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        return redirect('home')
     
     context = {'form': form, "topics": topics}
     return render(request, 'base/room_form.html', context)
@@ -143,11 +147,16 @@ def updateRoom(request, pk):
     
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+
+        return redirect('home')
     
-    context = {'form': form, "topics": topics}
+    context = {'form': form, "topics": topics, "room": room}
     return render(request, 'base/room_form.html', context)
 
 
@@ -170,7 +179,7 @@ def deleteRoom(request, pk):
 
 
 @login_required(login_url='login') # user cannot delete a message until he's logged-in
-def deleteMessage(request, pk):
+def deleteMessage(request, pk): 
     message = Message.objects.get(id=pk)
     
     if request.user != message.user:
