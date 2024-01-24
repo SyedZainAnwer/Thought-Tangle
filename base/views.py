f7337d80215db2b3bd28e7600f0b8bef49f9a177
Syed Zain Anwer <zainanwer24@gmail.com>
2024-01-24 02:40:31 +0100

    mobile responsive

--- base/views.py
+++ base/views.py
@@ -70,7 +70,7 @@ def home(request):
         Q(description__icontains=q)
     ) # query for room. we are gonna search by these queries
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5] # only getting the first 5 topics on the home page
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q)) # we will see only the messages of the related topic in that room
     
@@ -207,3 +207,18 @@ def updateUser(request):
     context = {"form": form}
     
     return render(request, 'base/update_user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    
+    context = {"topics": topics}
+    return render(request, 'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    
+    context = {"room_messages": room_messages}
+    return render(request, 'base/activity.html', context)
