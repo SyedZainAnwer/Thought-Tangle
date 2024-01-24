d34f5337d78b738b65d025bbda4276390afe117d
Syed Zain Anwer <zainanwer24@gmail.com>
2024-01-25 00:34:57 +0100

    intro to Django REST freamework

--- ThoughtTangle/urls.py
+++ ThoughtTangle/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 # urls trigger views and that what we see on the server
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')) # any path that matches and empty string, go to base.urls file
+    path('', include('base.urls')), # any path that matches and empty string, go to base.urls file
+    path('api', include('base.api.urls'))
 ]
